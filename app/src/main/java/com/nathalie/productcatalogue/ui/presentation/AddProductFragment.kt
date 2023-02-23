39565b94a40f07ed28ef222187713b1ccb19c58f
nathalie7980 <londonhippos@gmail.com>
2023-02-23 14:49:06 +0800

    added 'upload img' text in add product xml

--- app/src/main/java/com/nathalie/productcatalogue/ui/presentation/AddProductFragment.kt
+++ app/src/main/java/com/nathalie/productcatalogue/ui/presentation/AddProductFragment.kt
@@ -1,9 +1,12 @@
 package com.nathalie.productcatalogue.ui.presentation
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import com.nathalie.productcatalogue.R
 import com.nathalie.productcatalogue.data.api.RetrofitClient
@@ -13,6 +16,8 @@ import com.nathalie.productcatalogue.ui.viewModel.AddProductViewModel
 import com.nathalie.productcatalogue.ui.viewModel.HomeViewModel
 
 class AddProductFragment : BaseFragment<FragmentAddProductBinding>() {
+    private lateinit var filePickerLauncher: ActivityResultLauncher<String>
+    private var bytes: ByteArray? = null
     override val viewModel: AddProductViewModel by viewModels {
         AddProductViewModel.Provider(ProductRepository.getInstance(RetrofitClient.getInstance()))
     }
